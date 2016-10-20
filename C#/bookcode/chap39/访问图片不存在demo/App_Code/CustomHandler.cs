de2d0c6ef404abaf8996e200e1208d8ab0b0d9ee
lakeoffaith <905663280@qq.com>
2016-10-20 10:11:39 +0800

    Signed-off-by: lakeoffaith <905663280@qq.com>

---
+++ C#/bookcode/chap39/访问图片不存在demo/App_Code/CustomHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// CustomHandler 的摘要说明
+/// </summary>
+/// 
+namespace CustomHandler
+{
+    public class JpgHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            String tokenID = context.Request["tokenId"];
+            if (tokenID !=null )
+            {
+                context.Response.ContentType = "image/JPEG";
+                context.Response.WriteFile("/meinv.jpg");
+            }
+            else
+            {
+                String responseString = @"<!DOCTYPE html>
+                <html>
+                <head >
+                    <meta charset=""UTF-8"">
+                    <title>error</title>
+                </head>
+                <body>
+                    <div>
+                        <h1>申请tokenId后才能访问图片</h1>
+                    </div>
+                </body>
+                </html>
+                    ";
+                context.Response.ContentType = "text/html";
+                context.Response.Write(String.Format(responseString,context.Request.UserAgent));
+            }       
+		
+		 
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
